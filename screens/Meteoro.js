43d3b6a8e8c5250b4ff5e54508a1f229c3eb5825
Andréa J S França <dejandrea@gmail.com>
2022-05-30 01:48:21 -0300

    Aula 80

--- screens/Meteoro.js
+++ screens/Meteoro.js
@@ -1,10 +1,19 @@
 import axios from "axios";
 import React from "react";
-import { Text, View, StyleSheet, SafeAreaView, StatusBar, TouchableOpacity, Platform, ImageBackground, Image, Alert } from "react-native";
-import { styleProps } from "react-native-web/dist/cjs/modules/forwardedProps";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Alert,
+  FlatList,
+  Image,
+  ImageBackground,
+  Dimensions
+} from "react-native";
 
-const bgImage = require("../assets/meteor_bg.jpg")
-const MeteorIcon = require("../assets/meteor_icon.png")
 
 // UEG79o7VuN1AvC2nm6N7ze0clLmLOUaAHrKej06i
 //https://api.nasa.gov/neo/rest/v1/feed?api_key=DEMO_KEY
@@ -34,6 +43,43 @@ export default class MeteorScreen extends React.Component {
       })
   }
 
+  renderItem = ({ item }) => {
+    let meteor = item
+    let bg_img, speed, size;
+    if (meteor.threat_score <= 30) {
+      bg_img = require("../assets/meteor_bg1.png")
+      speed = require("../assets/meteor_speed3.gif")
+      size = 100
+    } else if (meteor.threat_score <= 75) {
+      bg_img = require("../assets/meteor_bg2.png")
+      speed = require("../assets/meteor_speed3.gif")
+      size = 150
+    } else {
+      bg_img = require("../assets/meteor_bg3.png")
+      speed = require("../assets/meteor_speed3.gif")
+      size = 200
+    }
+    return (
+      <View>
+        <ImageBackground source={bg_img} style={styles.backgroundImage}>
+          <View styles={styles.gifContainer}>
+            <Image source={speed} style={{ width: size, height: size, alignSelf: "center" }}></Image>
+            <View>
+              <Text style={[styles.cardTitle, { marginTop: 400, marginLeft: 50 }]}>{item.name}</Text>
+              <Text style={[styles.cardText, { marginTop: 20, marginLeft: 50 }]}>Mais Próximo da Terra - {item.close_approach_data[0].close_approach_date_full}</Text>
+              <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Diâmetro Mínimo (KM) - {item.estimated_diameter.kilometers.estimated_diameter_min}</Text>
+              <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Diâmetro Máximo (KM) - {item.estimated_diameter.kilometers.estimated_diameter_max}</Text>
+              <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Velocidade (KM/H) - {item.close_approach_data[0].relative_velocity.kilometers_per_hour}</Text>
+              <Text style={[styles.cardText, { marginTop: 5, marginLeft: 50 }]}>Distância da Terra (KM) - {item.close_approach_data[0].miss_distance.kilometers}</Text>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  };
+
+  keyExtractor = (item, index) => index.toString();
+
   render() {
     if (Object.keys(this.state.meteors).length === 0) {
       return (
@@ -44,81 +90,97 @@ export default class MeteorScreen extends React.Component {
         </View>
       )
     } else {
-      let meteor_arr = Object.keys(this.state.meteors).map(meteor_date =>{
+      let meteor_arr = Object.keys(this.state.meteors).map(meteor_date => {
         return this.state.meteors[meteor_date]
       })
-      let meteor = [].concat.apply([],meteor_arr)
+      let meteor = [].concat.apply([], meteor_arr)
 
       meteor.forEach(element => {
-        let diameter = (element.estimated_diameter.kilometers.estimated_diameter_mim + 
-          element.estimated_diameter.kilometers.estimated_diameter_max)/2
-        let threatScore = (diameter/element.close_approach_data[0].miss_distance.kilometers)*1000000000
+        let diameter = (element.estimated_diameter.kilometers.estimated_diameter_mim +
+          element.estimated_diameter.kilometers.estimated_diameter_max) / 2
+        let threatScore = (diameter / element.close_approach_data[0].miss_distance.kilometers) * 1000000000
         element.threat_score = threatScore
       });
+      meteor.sort(function (a, b) {
+        return b.threat_score - a.threat_score
+      })
+      meteor = meteor.slice(0, 5)
       return (
-        <View style={styles.conteiner}>
-
-          <Text>Tela Meteoro</Text>
-        </View>
-      )
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <FlatList
+            keyExtractor={this.keyExtractor}
+            data={meteor}
+            renderItem={this.renderItem}
+            horizontal={true}
+          />
+        </View >
+      );
     }
   }
 }
 
 const styles = StyleSheet.create({
-  conteiner: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+  container: {
+    flex: 1
   },
   droidSafeArea: {
     marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
   backgroundImage: {
     flex: 1,
-    resizeMode: "cover"
+    resizeMode: 'cover',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
   },
   titleBar: {
-    flex: 0.1,
+    flex: 0.15,
     justifyContent: "center",
     alignItems: "center"
   },
   titleText: {
-    fontWeight: "bold",
     fontSize: 30,
-    color: "white",
+    fontWeight: "bold",
+    color: "white"
   },
-  iconImage: {
-    height: 50,
-    width: 50,
+  meteorContainer: {
+    flex: 0.85
   },
-  mapContainer: {
-    flex: 0.6
+  listContainer: {
+    backgroundColor: 'rgba(52, 52, 52, 0.5)',
+    justifyContent: "center",
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 5,
+    borderRadius: 10,
+    padding: 10
   },
-  map: {
-    width: "100%",
-    height: "100%"
+  cardTitle: {
+    fontSize: 20,
+    marginBottom: 10,
+    fontWeight: "bold",
+    color: "white"
+  },
+  cardText: {
+    color: "white"
+  },
+  threatDetector: {
+    height: 10,
+    marginBottom: 10
+  },
+  gifContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1
+  },
+  meteorDataContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+
   },
   loading: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
-  },
-  infoContainer: {
-    flex: 0.2,
-    backgroundColor: "white",
-    marginTop: -10,
-    // borderTopLeftRadius: 30,
-    // borderTopRightRadius: 30,
-    padding: 30,
-    borderRadius: 30,
-    // alignItems:"center",
-    justifyContent: "center"
-  },
-  infoText: {
-    fontSize: 15,
-    color: "black",
-    fontWeight: "bold",
-    paddingTop: 10
   }
-})
+});
